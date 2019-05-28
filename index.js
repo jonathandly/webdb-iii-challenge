9c0a2e41e2d11a42d5d9171718f2a217731fc22d
Jonathan Daly <dalyj138@gmail.com>
2019-05-28 14:42:02 -0400

    Added POST and GET endpoints to index.js.

--- index.js
+++ index.js
@@ -11,3 +11,38 @@ const knexConfig = {
 };
 
 const db = knex(knexConfig);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+server.post('/api/cohorts', async (req, res) => {
+    try {
+        const [id] = await db('cohorts').insert(req.body);
+
+        const cohort = await db('cohorts')
+            .where({ id })
+            .first();
+        
+        res.status(201).json(cohort);
+    } catch(err) {
+        const message = errors[error.errno] || 'There is an error somewhere';
+        res.status(500).json({ message, err });
+    }
+});
+
+server.get('/api/cohorts', async (req, res) => {
+    try {
+        const cohorts = await db('cohorts'); // Get all cohorts
+        res.status(200).json(cohorts);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n** API running on http://localhost:${port} **\n`);
+});
