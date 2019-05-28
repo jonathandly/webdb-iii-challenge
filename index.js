496f5bb40b0c1c0c4153452f8eb58104c17705d7
Jonathan Daly <dalyj138@gmail.com>
2019-05-28 16:48:40 -0400

    Added PUT and DELETE endpoints in index.js.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.post('/api/cohorts', async (req, res) => {
         
         res.status(201).json(cohort);
     } catch(err) {
-        const message = errors[error.errno] || 'There is an error somewhere';
+        const message = 'There is an error somewhere';
         res.status(500).json({ message, err });
     }
 });
@@ -37,6 +37,72 @@ server.get('/api/cohorts', async (req, res) => {
     try {
         const cohorts = await db('cohorts'); // Get all cohorts
         res.status(200).json(cohorts);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.get('/api/cohorts/:id', async (req, res) => {
+    try {
+        const cohort = await db('cohorts')
+            .where({ id: req.params.id })
+            .first();
+        if(!cohort) {
+            res.status(404).json({ message: 'Cohort doesn\'t exist' });
+        } else {
+            res.status(200).json(cohort);
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.get('/api/cohorts/:id/students', async (req, res) => {
+    try {
+        const students = await db('students')
+            .where({ id: req.params.id })
+            .first();
+        if(!students) {
+            res.status(404).json({ message: 'No students in that cohort' });
+        } else {
+            res.status(200).json(students);
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.put('/api/cohorts/:id', async (req, res) => {
+    try {
+        const update = await db('cohorts')
+            .where({ id: req.params.id })
+            .update(req.body);
+
+        if(update > 0) {
+            const cohort = await db('cohorts')
+                .where({ id: req.params.id })
+                .first();
+
+            res.status(200).json(cohort);
+        } else {
+            res.status(404).json({ message: 'Cohort not found' });
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.delete('/api/cohorts/:id', async (req, res) => {
+    try {
+        const count = await db('cohorts')
+            .where({ id: req.params.id })
+            .del();
+
+        if(count > 0) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: 'Cohort not found' });
+        }
     } catch(err) {
         res.status(500).json(err);
     }
