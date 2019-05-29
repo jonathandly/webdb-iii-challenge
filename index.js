e86d19dc18acadb71f55aa823ea192d2d62a693b
Jonathan Daly <dalyj138@gmail.com>
2019-05-28 23:02:07 -0400

    Completed endpoints for /students route for the stretch problem.

--- index.js
+++ index.js
@@ -108,6 +108,82 @@ server.delete('/api/cohorts/:id', async (req, res) => {
     }
 });
 
+server.post('/api/students', async (req, res) => {
+    try {
+        const [id] = await db('students').insert(req.body);
+
+        const student = await db('students')
+            .where({ id })
+            .first();
+
+        res.status(201).json(student);
+    } catch(err) {
+        const message = 'There is an error somewhere';
+        res.status(500).json({ message, err });
+    }
+});
+
+server.get('/api/students', async (req, res) => {
+    try {
+        const students = await db('students'); // all the students from the table
+        res.status(200).json(students);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.get('/api/students/:id', async (req, res) => {
+    try {
+        const student = await db('students')
+            .where({ id: req.params.id })
+            .first();
+
+        if(!student) {
+            res.status(404).json({ message: 'Student couldn\'t be found' });
+        } else {
+            res.status(200).json(student);
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.put('/api/students/:id', async (req, res) => {
+    try {
+        const count = await db('students')
+            .where({ id: req.params.id })
+            .update(req.body);
+
+        if(count > 0) {
+            const student = await db('students')
+                .where({ id: req.params.id })
+                .first();
+
+            res.status(200).json(student);
+        } else {
+            res.status(404).json({ message: 'Student couldn\'t be found' });
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.delete('/api/students/:id', async (req, res) => {
+    try {
+        const count = await db('students')
+            .where({ id: req.params.id })
+            .del();
+
+        if(count > 0) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: 'Student not found' });
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n** API running on http://localhost:${port} **\n`);
--- seeds/00-cleanup.js
+++ seeds/00-cleanup.js
@@ -3,6 +3,11 @@ const cleaner = require('knex-cleaner');
 exports.seed = function(knex) {
   
   // cleans all tables and resets the primary keys
-  return cleaner.clean(knex); 
-
+  return cleaner.clean(knex, {
+    mode: "delete",
+    ignoreTables: [
+      "knex_migrations",
+      "knex_migrations_lock",
+    ],
+  });
 };
